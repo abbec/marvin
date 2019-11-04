58f9fb00d317ea2dd0d1b6bef5f9a9198ead4329
Albert Cervin <albert.cervin@goodbyekansas.com>
2019-11-04 16:59:03 +0100

    🎉 First real commit
    This sets up a simple 2D project that shows a green-ish screen.

---
+++ shell.nix
@@ -0,0 +1,37 @@
+let
+  nixpkgs = import <nixpkgs> {};
+in
+  with nixpkgs;
+  nixpkgs.mkShell {
+    name = "dev_shell";
+    buildInputs = [
+      rustc
+      cargo
+      rls
+      rustPackages.clippy
+      rustfmt
+
+      alsaLib
+      cmake
+      freetype
+      expat
+      openssl
+      pkgconfig
+      python3
+      vulkan-validation-layers
+      xlibs.libX11
+    ];
+
+    RUST_BACKTRACE = 1;
+    APPEND_LIBRARY_PATH = stdenv.lib.makeLibraryPath [
+      vulkan-loader
+      xlibs.libXcursor
+      xlibs.libXi
+      xlibs.libXrandr
+    ];
+
+    shellHook = ''
+      export PATH="~/.cargo/bin:$PATH"
+      export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$APPEND_LIBRARY_PATH"
+    '';
+  }
